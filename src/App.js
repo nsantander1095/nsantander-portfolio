f2681991aa7ecb0a5ceca7293ef19e4ded9b044e
Nick <nsantander1095@gmail.com>
2022-10-20 09:54:25 -0400

    full init, including file structure and beginnings of conditional render. Ready for bootstrap overhaul and to add the components we need

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+function renderPage(currentPage) {
+  if (currentPage === 'About') { 
+    return <About />
+  }
+  if (currentPage === 'Contact') { 
+    return <Contact />
+  }
+  if (currentPage === 'Portfolio') { 
+    return <Portfolio />
+  }
+  if (currentPage === 'Resume') { 
+    return <Resume />
+  }
+}
+
+function App() {
+  const [currentPage, setCurrentPage] = useState('About');
+  return (
+    <div>
+      <nav>
+        <a onClick={setCurrentPage('About')}>
+          About
+        </a>
+        <a onClick={setCurrentPage('Contact')}>
+          Contact
+        </a>
+        <a onClick={setCurrentPage('Portfolio')}>
+          Portfolio
+        </a>
+        <a onClick={setCurrentPage('Resume')}>
+          Resume
+        </a>
+      </nav>
+      {renderPage(currentPage)}
+    </div>
+  );
+}
+
+export default App;
