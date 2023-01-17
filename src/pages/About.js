07aa669fb76dad64dc4b8b0505b62cd0fa03fa3d
Nick <nsantander1095@gmail.com>
2023-01-17 14:22:14 -0500

    removed the old image and put in a square one, tried to make it look polished, need to mess around with the spacing

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import headshot from '../assets/images/headshot.png';
+import headshot from '../assets/images/headshotsqr.jpg';
 
 const About = () => {
   return (
     <div className="p-2 m-2 vh-100">
-      <img src={headshot} alt='headshot' className='rounded mx-auto' style={{width: 500}}/>
+      <img src={headshot} alt='headshot' className='rounded-circle mx-auto' style={{width: 300}}/>
       <h2 className="mb-3">About Me</h2>
       <p>
         Hello! My name is Nicolas Santander and this is my portfolio site! I am a recent graduate from the 2U Full Stack coding bootcamp that is run through UNCC. I am a Charlotte local and am looking to find work in backend web development.
