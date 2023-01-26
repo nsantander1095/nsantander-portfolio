c593a80bf7b7d6b72f964218e24d3d0770370711
Nick <nsantander1095@gmail.com>
2023-01-26 12:07:06 -0500

    added the first two cards, need to maybe looks back at the styled components to get them to be atyled, bootstrap isnt giving me enough control

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -6,9 +6,18 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
       <div className='card' style={{width: 500}}>
         <div className='card-body'>
-          <h5 className='card-title'>Text Editor PWA</h5>
-          <p className='card-text'>A fully functional progressive web application for any text editing needs.</p>
-          <a href='https://github.com/nsantander1095/pwa-text-editor-refactor-project' className='btn btn-info'>GitHub</a>
+          <h5 className='card-title'>Slo-Gram</h5>
+          <p className='card-text'>An Instagram clone using Handlebars for page views, Express/mySQL for the server, image hosting through Cloudinary, and image uploading through Multer node module.</p>
+          <p className='card-text'>My role on this collaborative project was centered heavily around the layout of the website with some backend work revolving around teh ability to add and remove friends.</p>
+          <a href='https://github.com/BBelk/Not-Insta' className='btn btn-info'>GitHub</a>
+        </div>
+      </div>
+      <div className='card' style={{width: 500}}>
+        <div className='card-body'>
+          <h5 className='card-title'>Memory Game</h5>
+          <p className='card-text'>Full MERN Stack application taking the form of a card flipping memory game. Relies on a globally state managed game engine that keeps track of scores, flips and any other pertinent metrics for the game.</p>
+          <p className='card-text'>My role on this collaborative project was mainly building out the layouts for the game as well as making it responsive. I also was heavily invovled in switching the game over to a gloabally state managed engine from the original structure.</p>
+          <a href='https://github.com/BBelk/Memory-Game' className='btn btn-info'>Github</a>
         </div>
       </div>
       <ul>
