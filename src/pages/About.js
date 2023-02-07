14ac7c2901ca3bc0c07c3abfa427c71c3d7d1bcc
Nick <nsantander1095@gmail.com>
2023-02-07 14:28:12 -0500

    changed the about a bit

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,7 @@ const About = () => {
           <img src={headshot} alt='headshot' className='rounded-circle' style={{width: '20rem'}}/>
         </div>
         <p className='text-center col-12 col-xxl-12' style={{width: '30rem'}}>
-          My name is Nicolas. I am a Full Stack web developer with a background in engineering and customer service. I was trained at the University of North Carolina in Charlotte and received a certificate in Full Stack Web Development. I received a Bachelors of Science in Engineering Physics with a Minor in Applied Math from the University of Colorado Boulder. Further, I have extensive high-end customer service skills.
+          My name is Nicolas. I am a Full Stack web developer with a background in engineering and customer service. I was trained at the University of North Carolina in Charlotte and received a certificate in Full Stack Web Development. I received a Bachelors of Science in Engineering Physics with a Minor in Applied Math from the University of Colorado Boulder. I have a passion for both frontend and backend web development, and I enjoy being part of a team.
         </p>
       </div>
     </div>
